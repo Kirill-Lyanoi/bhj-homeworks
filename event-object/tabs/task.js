7623d1b18497f3ea2c7c663713244dccd301bc0b
Kirill <117843865+Kirill-Lyanoi@users.noreply.github.com>
2024-08-27 14:42:23 +0300

    solution

---
+++ event-object/tabs/task.js
@@ -0,0 +1,12 @@
+const tabsNavigation = document.getElementById('tabs1')
+const tabs = Array.from(document.querySelectorAll('.tab'));
+const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
+tabsNavigation.addEventListener('click', (event) => {
+    const index= tabs.indexOf(event.target)
+    for (i = 0; i < tabs.length; i++) {
+       tabs[i].classList.remove('tab_active')
+       tabsContent[i].classList.remove('tab__content_active')
+    }
+    tabs[index].classList.add('tab_active')
+    tabsContent[index].classList.add('tab__content_active')
+})
