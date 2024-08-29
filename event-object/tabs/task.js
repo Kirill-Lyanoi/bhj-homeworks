93c6845dea85c8631ecb9df8b24fafc8e2b0f8a3
Kirill <117843865+Kirill-Lyanoi@users.noreply.github.com>
2024-08-29 12:51:59 +0300

    correction

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,12 +1,15 @@
 const tabsNavigation = document.getElementById('tabs1')
 const tabs = Array.from(document.querySelectorAll('.tab'));
 const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
-tabsNavigation.addEventListener('click', (event) => {
-    const index= tabs.indexOf(event.target)
-    for (i = 0; i < tabs.length; i++) {
-       tabs[i].classList.remove('tab_active')
-       tabsContent[i].classList.remove('tab__content_active')
-    }
-    tabs[index].classList.add('tab_active')
-    tabsContent[index].classList.add('tab__content_active')
+tabs.forEach((tab)=> {
+tab.addEventListener('click', (event) => {
+        const index= tabs.indexOf(event.target)
+        for (let i = 0; i < tabs.length; i++) {
+           tabs[i].classList.remove('tab_active')
+           tabsContent[i].classList.remove('tab__content_active')
+        }
+        tabs[index].classList.add('tab_active')
+        tabsContent[index].classList.add('tab__content_active')
+    })
+    
 })
