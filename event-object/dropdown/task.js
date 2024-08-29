ce3f137597f46c5113aa457b6a70fe179fe9d8d2
Kirill <117843865+Kirill-Lyanoi@users.noreply.github.com>
2024-08-29 12:50:45 +0300

    correction

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,4 +1,4 @@
-let dropdown = document.querySelector(".dropdown");
+let dropdown = document.querySelector(".dropdown__value");
 let dropdownList = document.querySelector(".dropdown__list");
 const dropdownLinks = Array.from(document.querySelectorAll(".dropdown__link"));
 
@@ -12,12 +12,8 @@ dropdownLinks.forEach((link) => {
     link.addEventListener("click", (e) => {
         e.preventDefault()
         const targetItem = e.target; 
-        const linkItem = targetItem.closest('.dropdown__item')
-         if (linkItem === null) {
-            e.stopPropagation()
-        } else {
-        document.querySelector(".dropdown__value").textContent = targetItem.textContent
-        }
+        document.querySelector(".dropdown__value").textContent = targetItem.textContent;
+        dropDownActive ()
     })
 })
 
